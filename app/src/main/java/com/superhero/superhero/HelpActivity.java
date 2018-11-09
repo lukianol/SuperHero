5b694e60566dec438b4e5e00a3cf4af4f101cef5
d.lukiantsev <dnsanche@gmail.com>
2018-11-08 22:34:00 -0800

    Help activity

--- app/src/main/java/com/superhero/superhero/HelpActivity.java
+++ app/src/main/java/com/superhero/superhero/HelpActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -17,10 +18,13 @@ public class HelpActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final EditText titleEntered = findViewById(R.id.title_entered);
+        final EditText descEntered = findViewById(R.id.description_entered);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, titleEntered.getText().toString() + descEntered.getText().toString(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
