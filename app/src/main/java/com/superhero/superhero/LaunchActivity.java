da197cc4a4573ebdce842e72d26abb5c47ad477b
Alex Lukiantsev <lukiao@amazon.com>
2018-11-23 21:50:46 -0800

    Add notifications

--- app/src/main/java/com/superhero/superhero/LaunchActivity.java
+++ app/src/main/java/com/superhero/superhero/LaunchActivity.java
@@ -3,11 +3,15 @@ package com.superhero.superhero;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
         NavigationManager navigator = new NavigationManager();
 
         if (new IdentityManager().isLoggedIn()) {
