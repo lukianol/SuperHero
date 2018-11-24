f6f8a1cc002102af777c0e0d05e1eb13dea51ba3
Alex Lukiantsev <lukiao@amazon.com>
2018-11-23 22:32:03 -0800

    notification toast

--- app/src/main/java/com/superhero/superhero/notifications/firebase/SuperHeroMessagingService.java
+++ app/src/main/java/com/superhero/superhero/notifications/firebase/SuperHeroMessagingService.java
@@ -1,6 +1,8 @@
 package com.superhero.superhero.notifications.firebase;
 
-import android.util.Log;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -8,12 +10,12 @@ import com.google.firebase.messaging.RemoteMessage;
 public class SuperHeroMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FCM Service";
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO: Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated.
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    public void onMessageReceived(final RemoteMessage remoteMessage) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(), remoteMessage.getNotification().getTitle() + remoteMessage.getNotification().getBody(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
